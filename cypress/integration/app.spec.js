509fbce00a5c04137e76ba9ec4250bd3c427c6f1
Charles Hoffman <charles.d.hoffman@accenture.com>
2019-12-04 15:05:58 -0600

    bring in some more ui elements

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,9 +3,9 @@ import Chance from "chance";
 
 const chance = new Chance();
 
-const verifyInputField = selector => {
+const verifyInputField = field => {
     const expectedInputValue = chance.string();
-    cy.get(selector)
+    field
         .type("{selectall}{del}")
         .type(expectedInputValue)
         .should("have.value", expectedInputValue);
@@ -16,7 +16,19 @@ context("Customer Details", () => {
         cy.visit("/");
     });
 
-    it("has a message input", () => {
-        verifyInputField(".message>input");
+    const messageInput = () => cy.get('.message>input');
+    const sendButton = () => cy.get('.message>button');
+    const messageHistoryView = () => cy.get('.history>textarea');
+
+    it("has a working message input", () => {
+        verifyInputField(messageInput());
+    });
+
+    it("and a send button", () => {
+        expect(sendButton()).to.not.be.null;
+    });
+
+    it("and a message history text area", () => {
+        expect(messageHistoryView()).to.not.be.null;
     });
 });
--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div className="message">
-        <input />
-      </div>
-  );
+    return (
+        <div>
+            <div className="history">
+                <textarea/>
+            </div>
+            <div className="message">
+                <input/>
+                <button/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
