e25c265397908b40453994fde6e764a942f0903c
Charles Hoffman <charles.d.hoffman@accenture.com>
2019-12-09 16:04:17 -0600

    WIP: test connecting to backend

--- src/ChatAction.js
+++ src/ChatAction.js
@@ -1,4 +1,13 @@
+import {connect} from "@giantmachines/redux-websocket/dist";
 export const SEND_MESSAGE = "SEND_MESSAGE";
+
+export const connectToServer = () => {
+    return (dispatch, getState) => {
+        if (!getState().connected) {
+            dispatch(connect("ws://localhost:8080"));
+        }
+    }
+};
 
 export const sendMessage = (message) => {
     return dispatch => {
--- src/ChatAction.spec.js
+++ src/ChatAction.spec.js
@@ -1,10 +1,17 @@
+import * as sinon from "sinon";
 import Chance from 'chance';
-import {SEND_MESSAGE, sendMessage} from "./ChatAction";
-
+import {connectToServer, SEND_MESSAGE, sendMessage} from "./ChatAction";
 const chance = new Chance();
-const dispatchSpy = jest.fn();
+const dispatchSpy = sinon.stub();
 
 describe("Chat Action Creator", () => {
+
+    let getStateStub;
+
+    beforeEach(() => {
+        getStateStub = () => ({connected: false});
+    });
+
     test("should dispatch action when message sent", () => {
         const message = chance.string();
 
@@ -15,6 +22,18 @@ describe("Chat Action Creator", () => {
 
         sendMessage(message)(dispatchSpy);
 
-        expect(dispatchSpy).toHaveBeenCalledWith(expectedAction);
+        expect(dispatchSpy.calledWithExactly(expectedAction)).toEqual(true);
+    });
+
+    test("should connect to store when not connected", () => {
+        connectToServer()(dispatchSpy, getStateStub);
+
+        expect(dispatchSpy.args[0][0].type).toEqual("REDUX_WEBSOCKET::CONNECT");
+    });
+
+    test("should not connect to store when connected", () => {
+        connectToServer()(dispatchSpy, () => ({connected: true}));
+
+        expect(dispatchSpy.called).toBe(false);
     });
 });
