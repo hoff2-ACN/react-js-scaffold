a1dfb216f6133f7af490fa549c4da02ba4c0c1b3
Charles Hoffman <charles.d.hoffman@accenture.com>
2019-12-09 16:03:26 -0600

    Failing acceptance test for sending message to backend

--- cypress/integration/Server.spec.js
+++ cypress/integration/Server.spec.js
@@ -7,7 +7,6 @@ describe('the chat server', () => {
 
     it('gives a chat history', () => {
         const expectedMessage = chance.string();
-
         const json = {"message": expectedMessage};
 
         cy.request('POST', url, json);
@@ -19,6 +18,22 @@ describe('the chat server', () => {
         cy.request('GET', url)
             .its('body')
             .should('have.length', 1)
+            .should('include', expectedMessage);
+    });
+
+    it('makes the messages you send available to others', () => {
+        const expectedMessage = chance.string();
+        const messageInput = () => cy.get('.message input');
+        const sendButton = () => cy.get('.message button');
+
+        cy.visit('/');
+        messageInput()
+            .type("{selectall}{del}")
+            .type(expectedMessage);
+        sendButton().click();
+
+        cy.request('GET', url)
+            .its('body')
             .should('include', expectedMessage);
     });
 });
